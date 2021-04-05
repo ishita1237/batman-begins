33c5c2d5c1f2f26dbb4e85dbeab1317b59fc9baa
ishita1237 <76168572+ishita1237@users.noreply.github.com>
2021-04-05 19:27:31 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -3,7 +3,7 @@ class umbrella {
         var options = {
             isStatic: true,
         }
-        this.animation = loadAnimation("Walking Frame/walking_1.png","Walking Frame/walking_2.png","Walking Frame/walking_3.png","Walking Frame/walking_4.png","Walking Frame/walking_5.png","Walking Frame/walking_6.png","Walking Frame/walking_7.png","Walking Frame/walking_8.png");
+        this.animation = loadImage("Walking Frame/walking_1.png");
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella)
@@ -11,7 +11,7 @@ class umbrella {
 
     display(){
         var pos = this.umbrella.position;
-        animationMode(CENTER);
-        animation(this.animation,pos.x,pos.y+70,300,300);
+        imageMode(CENTER);
+        image(this.animation,pos.x,pos.y+70,300,300);
     }
 }
